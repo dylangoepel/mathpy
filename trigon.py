e8a42416de5dd619545fabfbf0519a7f71da9c93
Dylan Göpel <dylan.connor@t-online.de>
2020-12-12 19:55:54 +0100

    add arc functions

--- trigon.py
+++ trigon.py
@@ -1,9 +1,15 @@
 # trigonometric functions
 
 import exp
+import inverse
 from complex import Complex
 
 sin = lambda x: exp.exp(Complex(0, x)).imag
 cos = lambda x: exp.exp(Complex(0, x)).real
 
 tan = lambda x: sin(x) / cos(x)
+
+arcsin = inverse.inverse(sin)
+arccos = inverse.inverse(cos)
+
+arctan = inverse.inverse(tan)
