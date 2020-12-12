da260e47eb2ac27d93e36800c8d5b760bd87e922
Dylan Göpel <dylan.connor@t-online.de>
2020-12-12 19:36:53 +0100

    remove external libraries

--- binom.py
+++ binom.py
@@ -1,8 +1,5 @@
 # approximate the binomial distribution the binomial distribution
 
-import numpy as np
-import matplotlib.pyplot as plt
-
 def binom(a, b):
     p = 1
     for i in range(a - b + 1, a + 1):
@@ -11,18 +8,6 @@ def binom(a, b):
         p //= i
     return p
 
-def binomGraph(samples):
-    step = 1 / samples
-    xs = np.arange(0, 1 + step, step)
-    ys = xs.copy()
-    for i in range(samples):
-        ys[i] = binom(samples, i)
-    return xs, ys
-
 def plotBinom(samples):
     x, y = binomGraph(samples)
     plt.plot(x, y)
-
-if __name__ == "__main__":
-    plotBinom(1000)
-    plt.show()
